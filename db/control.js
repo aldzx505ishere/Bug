9f2a391f5deb85311033891b8fe76debf3d7a952
aldzx505ishere <duaorangs8@gmail.com>
2026-01-02 18:33:34 +0700

    Add files via upload

---
+++ db/control.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8274795921:AAEE2dh4QpmwZAs8ZA1oSCJXeyk7GUGiC10", 
+  owner: "8313801625", 
+  port: "1208", // ubah port panel lu
+  ipvps: "http://arzz.buyerpenxzy.biz.id" // ubah port panel lu
+};
